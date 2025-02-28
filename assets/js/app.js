d6601c4f1cd358e04afcb97ed90ecc6f0cef5729
AmitSheoran-557 <amit22082006@gmail.com>
2025-02-28 14:41:28 +0530

    add duration and make responsive

--- assets/js/app.js
+++ assets/js/app.js
@@ -54,7 +54,7 @@ mm.add("(min-width:1600.98px)", () => {
     ScrollTrigger.create({
         trigger: ".section-1",
         start: "top top",
-        end: "100%",
+        end: "400%",
         pin: true,
         animation: tl,
         scrub: 2,
@@ -65,84 +65,101 @@ mm.add("(min-width:1600.98px)", () => {
         opacity: 0,
     }, {
         opacity: 1,
-        duration: 5,
-    }).fromTo(".triangle-img", {
+
+    }, 0);
+
+    tl.fromTo(".triangle-img", {
         bottom: "-50%",
     }, {
         bottom: "-40%",
         duration: 10,
-    }).fromTo(".right-arrow-img", {
+    }, 0.2);
+
+
+    tl.fromTo(".right-arrow-img", {
         right: '-30%',
         bottom: '-100%',
     }, {
         bottom: '5%',
-        duration: 100,
-    }).fromTo(".button-1", {
+        duration: 20,
+    });
+    tl.fromTo(".button-1", {
         rotate: "0deg",
         backgroundColor: "#7EE49C80",
     }, {
         rotate: "-7deg",
         duration: 25,
         backgroundColor: "#53849980",
-    }).fromTo(".button-1", {
+    });
+    tl.fromTo(".button-1", {
         rotate: "-7deg",
         backgroundColor: "#53849980",
     }, {
         rotate: "0deg",
         duration: 25,
         backgroundColor: "#7EE49C80",
-    }).fromTo(".content-img-1", {
+    });
+    tl.fromTo(".content-img-1", {
         opacity: 1,
     }, {
         opacity: 0,
         duration: 20,
-    }).fromTo(".triangle-img", {
+    });
+    tl.fromTo(".triangle-img", {
         bottom: "-40%",
     }, {
         bottom: "-30%",
         duration: 10,
-    }).fromTo(".right-arrow-img", {
+    });
+    tl.fromTo(".right-arrow-img", {
         right: '-30%',
         bottom: '5%',
     }, {
         bottom: '20%',
         duration: 30,
-    }).fromTo(".button-2", {
+    });
+    tl.fromTo(".button-2", {
         rotate: "0deg",
         backgroundColor: "#7EE49C80",
     }, {
         rotate: "-7deg",
         duration: 25,
         backgroundColor: "#53849980",
-    }).fromTo(".content-img-2", {
+    });
+    tl.fromTo(".content-img-2", {
         opacity: 0,
     }, {
         opacity: 1,
         duration: 30,
-    }).fromTo(".content-img-2", {
+    });
+    tl.fromTo(".content-img-2", {
         opacity: 1,
     }, {
         opacity: 0,
         duration: 30,
-    }).fromTo(".button-2", {
+    });
+    tl.fromTo(".button-2", {
         rotate: "-7deg",
         backgroundColor: "#53849980",
     }, {
         rotate: "0deg",
         duration: 25,
         backgroundColor: "#7EE49C80",
-    }).fromTo(".triangle-img", {
+    });
+    tl.fromTo(".triangle-img", {
         bottom: "-30%",
     }, {
         bottom: "-20%",
         duration: 25,
-    }).fromTo(".right-arrow-img", {
+    });
+    tl.fromTo(".right-arrow-img", {
         right: '-30%',
         bottom: '20%',
     }, {
         bottom: '35%',
         duration: 30,
-    }).fromTo(".button-3", {
+    });
+    tl.fromTo(".button-3", {
         rotate: "0deg",
         backgroundColor: "#7EE49C80",
     }, {
@@ -150,77 +167,90 @@ mm.add("(min-width:1600.98px)", () => {
         duration: 25,
         backgroundColor: "#53849980",
 
-    }).fromTo(".content-img-3", {
+    });
+    tl.fromTo(".content-img-3", {
         opacity: 0,
     }, {
         opacity: 1,
         duration: 25,
-    }).fromTo(".content-img-3", {
+    });
+    tl.fromTo(".content-img-3", {
         opacity: 1,
     }, {
         opacity: 0,
         duration: 10,
-    }).fromTo(".button-3", {
+    });
+    tl.fromTo(".button-3", {
         rotate: "-7deg",
         backgroundColor: "#53849980",
     }, {
         rotate: "0deg",
         duration: 25,
         backgroundColor: "#7EE49C80",
-    }).fromTo(".triangle-img", {
+    });
+    tl.fromTo(".triangle-img", {
         bottom: "-20%",
     }, {
         bottom: "-10%",
         duration: 25,
-    }).fromTo(".right-arrow-img", {
+    });
+    tl.fromTo(".right-arrow-img", {
         right: '-30%',
         bottom: '35%',
     }, {
         bottom: '50%',
         duration: 30,
-    }).fromTo(".button-4", {
+    });
+    tl.fromTo(".button-4", {
         rotate: "0deg",
         backgroundColor: "#7EE49C80",
     }, {
         rotate: "-7deg",
         duration: 25,
         backgroundColor: "#53849980",
-    }).fromTo(".content-img-4", {
+    });
+    tl.fromTo(".content-img-4", {
         opacity: 0,
     }, {
         opacity: 1,
         duration: 25,
-    }).fromTo(".content-img-4", {
+    });
+    tl.fromTo(".content-img-4", {
         opacity: 1,
     }, {
         opacity: 0,
         duration: 10,
-    }).fromTo(".button-4", {
+    });
+    tl.fromTo(".button-4", {
         rotate: "-7deg",
         backgroundColor: "#53849980",
     }, {
         rotate: "0deg",
         duration: 25,
         backgroundColor: "#7EE49C80",
-    }).fromTo(".triangle-img", {
+    });
+    tl.fromTo(".triangle-img", {
         bottom: "-10%",
     }, {
         bottom: "0",
         duration: 25,
-    }).fromTo(".right-arrow-img", {
+    });
+    tl.fromTo(".right-arrow-img", {
         right: '-30%',
         bottom: '50%',
     }, {
         bottom: '65%',
         duration: 30,
-    }).fromTo(".button-5", {
+    });
+    tl.fromTo(".button-5", {
         rotate: "0deg",
         backgroundColor: "#7EE49C80",
     }, {
         rotate: "-7deg",
         duration: 25,
         backgroundColor: "#53849980",
-    }).fromTo(".content-img-5", {
+    });
+    tl.fromTo(".content-img-5", {
         opacity: 0,
     }, {
         opacity: 2,
@@ -274,14 +304,14 @@ mm.add("(min-width:1280.98px) and (max-width:1600.98px)", () => {
         bottom: "-50%",
     })
     gsap.set(".right-arrow-img", {
-        right: '-15%',
+        right: '-20%',
         bottom: '-100%',
     })
 
     ScrollTrigger.create({
         trigger: ".section-1",
         start: "top top",
-        end: "100%",
+        end: "400%",
         pin: true,
         animation: tl,
         scrub: 2,
@@ -292,84 +322,101 @@ mm.add("(min-width:1280.98px) and (max-width:1600.98px)", () => {
         opacity: 0,
     }, {
         opacity: 1,
-        duration: 5,
-    }).fromTo(".triangle-img", {
+
+    }, 0);
+
+    tl.fromTo(".triangle-img", {
         bottom: "-50%",
     }, {
         bottom: "-40%",
         duration: 10,
-    }).fromTo(".right-arrow-img", {
-        right: '-30%',
+    }, 0.2);
+
+
+    tl.fromTo(".right-arrow-img", {
+        right: '-20%',
         bottom: '-100%',
     }, {
         bottom: '5%',
-        duration: 100,
-    }).fromTo(".button-1", {
+        duration: 20,
+    });
+    tl.fromTo(".button-1", {
         rotate: "0deg",
         backgroundColor: "#7EE49C80",
     }, {
         rotate: "-7deg",
         duration: 25,
         backgroundColor: "#53849980",
-    }).fromTo(".button-1", {
+    });
+    tl.fromTo(".button-1", {
         rotate: "-7deg",
         backgroundColor: "#53849980",
     }, {
         rotate: "0deg",
         duration: 25,
         backgroundColor: "#7EE49C80",
-    }).fromTo(".content-img-1", {
+    });
+    tl.fromTo(".content-img-1", {
         opacity: 1,
     }, {
         opacity: 0,
         duration: 20,
-    }).fromTo(".triangle-img", {
+    });
+    tl.fromTo(".triangle-img", {
         bottom: "-40%",
     }, {
         bottom: "-30%",
         duration: 10,
-    }).fromTo(".right-arrow-img", {
-        right: '-15%',
+    });
+    tl.fromTo(".right-arrow-img", {
+        right: '-20%',
         bottom: '5%',
     }, {
         bottom: '20%',
         duration: 30,
-    }).fromTo(".button-2", {
+    });
+    tl.fromTo(".button-2", {
         rotate: "0deg",
         backgroundColor: "#7EE49C80",
     }, {
         rotate: "-7deg",
         duration: 25,
         backgroundColor: "#53849980",
-    }).fromTo(".content-img-2", {
+    });
+    tl.fromTo(".content-img-2", {
         opacity: 0,
     }, {
         opacity: 1,
         duration: 30,
-    }).fromTo(".content-img-2", {
+    });
+    tl.fromTo(".content-img-2", {
         opacity: 1,
     }, {
         opacity: 0,
         duration: 30,
-    }).fromTo(".button-2", {
+    });
+    tl.fromTo(".button-2", {
         rotate: "-7deg",
         backgroundColor: "#53849980",
     }, {
         rotate: "0deg",
         duration: 25,
         backgroundColor: "#7EE49C80",
-    }).fromTo(".triangle-img", {
+    });
+    tl.fromTo(".triangle-img", {
         bottom: "-30%",
     }, {
         bottom: "-20%",
         duration: 25,
-    }).fromTo(".right-arrow-img", {
-        right: '-15%',
+    });
+    tl.fromTo(".right-arrow-img", {
+        right: '-20%',
         bottom: '20%',
     }, {
         bottom: '35%',
         duration: 30,
-    }).fromTo(".button-3", {
+    });
+    tl.fromTo(".button-3", {
         rotate: "0deg",
         backgroundColor: "#7EE49C80",
     }, {
@@ -377,77 +424,90 @@ mm.add("(min-width:1280.98px) and (max-width:1600.98px)", () => {
         duration: 25,
         backgroundColor: "#53849980",
 
-    }).fromTo(".content-img-3", {
+    });
+    tl.fromTo(".content-img-3", {
         opacity: 0,
     }, {
         opacity: 1,
         duration: 25,
-    }).fromTo(".content-img-3", {
+    });
+    tl.fromTo(".content-img-3", {
         opacity: 1,
     }, {
         opacity: 0,
         duration: 10,
-    }).fromTo(".button-3", {
+    });
+    tl.fromTo(".button-3", {
         rotate: "-7deg",
         backgroundColor: "#53849980",
     }, {
         rotate: "0deg",
         duration: 25,
         backgroundColor: "#7EE49C80",
-    }).fromTo(".triangle-img", {
+    });
+    tl.fromTo(".triangle-img", {
         bottom: "-20%",
     }, {
         bottom: "-10%",
         duration: 25,
-    }).fromTo(".right-arrow-img", {
-        right: '-15%',
+    });
+    tl.fromTo(".right-arrow-img", {
+        right: '-20%',
         bottom: '35%',
     }, {
         bottom: '50%',
         duration: 30,
-    }).fromTo(".button-4", {
+    });
+    tl.fromTo(".button-4", {
         rotate: "0deg",
         backgroundColor: "#7EE49C80",
     }, {
         rotate: "-7deg",
         duration: 25,
         backgroundColor: "#53849980",
-    }).fromTo(".content-img-4", {
+    });
+    tl.fromTo(".content-img-4", {
         opacity: 0,
     }, {
         opacity: 1,
         duration: 25,
-    }).fromTo(".content-img-4", {
+    });
+    tl.fromTo(".content-img-4", {
         opacity: 1,
     }, {
         opacity: 0,
-        duration: 10,
-    }).fromTo(".button-4", {
+        duration: 25,
+    });
+    tl.fromTo(".button-4", {
         rotate: "-7deg",
         backgroundColor: "#53849980",
     }, {
         rotate: "0deg",
         duration: 25,
         backgroundColor: "#7EE49C80",
-    }).fromTo(".triangle-img", {
+    });
+    tl.fromTo(".triangle-img", {
         bottom: "-10%",
     }, {
         bottom: "0",
         duration: 25,
-    }).fromTo(".right-arrow-img", {
-        right: '-15%',
+    });
+    tl.fromTo(".right-arrow-img", {
+        right: '-20%',
         bottom: '50%',
     }, {
         bottom: '65%',
         duration: 30,
-    }).fromTo(".button-5", {
+    });
+    tl.fromTo(".button-5", {
         rotate: "0deg",
         backgroundColor: "#7EE49C80",
     }, {
         rotate: "-7deg",
         duration: 25,
         backgroundColor: "#53849980",
-    }).fromTo(".content-img-5", {
+    });
+    tl.fromTo(".content-img-5", {
         opacity: 0,
     }, {
         opacity: 2,
